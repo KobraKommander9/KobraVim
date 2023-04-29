297fdf1f46e8fe6398961369b83416e438f81d75
Ed <ed.kilgore@tcnbroadcasting.com>
2023-04-28 22:13:49 -0600

    Update heirline config

--- lua/kobra/plugins/ui.lua
+++ lua/kobra/plugins/ui.lua
@@ -55,11 +55,13 @@ M[#M+1] = {
 M[#M+1] = {
   'rebelot/heirline.nvim',
   event = 'VeryLazy',
-  opts = {
-    statusline = require('kobra.config.ui.lines.statusline').statusline(),
-    winbar = require('kobra.config.ui.lines.winbar').winbar(),
-    -- tabline = require('kobra.config.ui.lines.tabline').tabline(),
-  },
+  config = function()
+    require('heirline').setup({
+      statusline = require('kobra.config.ui.lines.statusline').statusline(),
+      winbar = require('kobra.config.ui.lines.winbar').winbar(),
+      -- tabline = require('kobra.config.ui.lines.tabline').tabline(),
+    })
+  end,
 }
 
 M[#M+1] = {
