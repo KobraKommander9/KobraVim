1d9ee81f1bd47c3159631032b9d6e73ddda1276c
Ed <ed.kilgore@tcnbroadcasting.com>
2023-04-28 21:59:37 -0600

    Remove Semver temporarily for testing

--- lua/kobra/plugins/core.lua
+++ lua/kobra/plugins/core.lua
@@ -2,5 +2,5 @@ require('kobra.core').init()
 
 return {
   { 'folke/lazy.nvim', version = '*' },
-  { 'KobraKommander9/KobraVim', priority = 10000, lazy = false, config = true, cond = true, version = '*' },
+  { 'KobraKommander9/KobraVim', priority = 10000, lazy = false, config = true, cond = true, version = false },
 }
