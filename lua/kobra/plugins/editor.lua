3da3d2e6cade7ffedfa5ff7b8e4a935a30ef1e48
Ed <ed.kilgore@tcnbroadcasting.com>
2023-04-29 07:50:22 -0600

    Update telescope

--- lua/kobra/plugins/editor.lua
+++ lua/kobra/plugins/editor.lua
@@ -103,7 +103,7 @@ M[#M+1] = {
     }
 
     local layouts = require('kobra.core').layouts
-    vim.notiy('layouts.colemak: ' .. tostring(layouts.colemak))
+    vim.notify('layouts.colemak: ' .. tostring(layouts.colemak))
     local n, p, j, k
     if layouts.colemak then
       n, p, j, k = 'j', 'k', 'n', 'e'
