5d8b72f1f6842d105c474f37284cda4483006f70
Ed <ed.kilgore@tcnbroadcasting.com>
2023-04-29 07:38:43 -0600

    Update FileBrowser

--- lua/kobra/plugins/editor.lua
+++ lua/kobra/plugins/editor.lua
@@ -148,7 +148,7 @@ M[#M+1] = {
 -- file browser
 M[#M+1] = {
   'telescope.nvim',
-  event = { 'DirChangedPre', 'BufNewFile' },
+  event = 'VimEnter',
   dependencies = {
     {
       'nvim-telescope/telescope-file-browser.nvim',
