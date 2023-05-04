82a08b0ebe2d0b3ecf5e895083899e3b5e8c9b52
Ed <ed.kilgore@tcnbroadcasting.com>
2023-05-04 17:21:52 -0600

    Unlazy load telescope

--- lua/kobra/plugins/editor.lua
+++ lua/kobra/plugins/editor.lua
@@ -31,8 +31,9 @@ M[#M + 1] = {
 -- fuzzy finder and file browser
 M[#M + 1] = {
 	"nvim-telescope/telescope.nvim",
-	cmd = "Telescope",
-	event = "VimEnter",
+	lazy = false,
+	-- cmd = "Telescope",
+	-- event = "VimEnter",
 	version = false,
 	dependencies = {
 		{ "nvim-telescope/telescope-file-browser.nvim" },
