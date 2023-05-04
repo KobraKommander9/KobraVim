573cfe49ab4586c87bced3208f59408f667deb07
Ed <ed.kilgore@tcnbroadcasting.com>
2023-05-04 08:32:03 -0600

    Fix lsp keymaps and presets

--- lua/kobra/plugins/presets/kobra/init.lua
+++ lua/kobra/plugins/presets/kobra/init.lua
@@ -1,6 +1,6 @@
 return {
-  { import = 'kobra.plugins.extras.buffers' },
-  { import = 'kobra.plugins.extras.coding' },
-  { import = 'kobra.plugins.extras.formatting' },
-  { import = 'kobra.plugins.extras.lang' },
+	{ import = "kobra.plugins.extras.coding" },
+	{ import = "kobra.plugins.extras.editor" },
+	{ import = "kobra.plugins.extras.formatting" },
+	{ import = "kobra.plugins.extras.lang" },
 }
