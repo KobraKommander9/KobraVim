5ff23493c02f7621a6c3946837004bada2289c30
Ed <ed.kilgore@tcnbroadcasting.com>
2023-05-05 07:24:29 -0600

    Give go it's own formatting group

--- lua/kobra/plugins/extras/lang/go.lua
+++ lua/kobra/plugins/extras/lang/go.lua
@@ -47,7 +47,7 @@ M[#M + 1] = {
 		require("kobra.util").on_attach(function(client, buffer)
 			if client.name == "gopls" then
 				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = vim.api.nvim_create_augroup("LspFormat." .. buffer, { clear = true }),
+					group = vim.api.nvim_create_augroup("GoImport." .. buffer, { clear = true }),
 					buffer = buffer,
 					callback = function()
 						require("go.format").goimport()
