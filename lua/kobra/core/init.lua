083c73667d10ce9a0abe0fd9a42eb0f39406005a
Ed <ed.kilgore@tcnbroadcasting.com>
2023-05-05 07:36:45 -0600

    Update setup

--- lua/kobra/core/init.lua
+++ lua/kobra/core/init.lua
@@ -97,6 +97,10 @@ M.renames = {
 local options
 
 function M.setup(opts)
+	if opts.start_screen and opts.start_screen.folders then
+		options.start_screen.folders = opts.start_screen.folders
+	end
+
 	options = vim.tbl_deep_extend("force", defaults, opts or {})
 	if not M.has() then
 		require("lazy.core.util").error(
