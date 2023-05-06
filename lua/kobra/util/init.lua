4d1579d5d02f4c11a6cc084ed319d0cb22d9054f
Ed Kilgore <118004611+KobraKommander9@users.noreply.github.com>
2023-05-06 15:06:27 -0600

    Update keymaps (#27)
    Co-authored-by: Ed <ed.kilgore@tcnbroadcasting.com>

--- lua/kobra/util/init.lua
+++ lua/kobra/util/init.lua
@@ -1,144 +1,186 @@
 local M = {}
 
-M.root_patterns = { '.git', 'lua' }
+local Util = require("lazy.core.util")
+
+M.root_patterns = { ".git", "lua" }
 
 function M.on_attach(on_attach)
-  vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-      local buffer = args.buf
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      on_attach(client, buffer)
-    end
-  })
+	vim.api.nvim_create_autocmd("LspAttach", {
+		callback = function(args)
+			local buffer = args.buf
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			on_attach(client, buffer)
+		end,
+	})
 end
 
 function M.has(plugin)
-  return require('lazy.core.config').plugins[plugin] ~= nil
+	return require("lazy.core.config").plugins[plugin] ~= nil
 end
 
 function M.on_very_lazy(fn)
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'VeryLazy',
-    callback = function()
-      fn()
-    end,
-  })
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "VeryLazy",
+		callback = function()
+			fn()
+		end,
+	})
 end
 
 -- delay notifications until vim.notify is available or after 500ms
 function M.lazy_notify()
-  local notifs = {}
-  local function temp(...)
-    table.insert(notifs, vim.F.pack_len(...))
-  end
+	local notifs = {}
+	local function temp(...)
+		table.insert(notifs, vim.F.pack_len(...))
+	end
 
-  local orig = vim.notify
-  vim.notify = temp
+	local orig = vim.notify
+	vim.notify = temp
 
-  local timer = vim.loop.new_timer()
-  local check = vim.loop.new_check()
+	local timer = vim.loop.new_timer()
+	local check = vim.loop.new_check()
 
-  local replay = function()
-    timer:stop()
-    check:stop()
-    if vim.notify == temp then
-      vim.notify = orig
-    end
+	local replay = function()
+		timer:stop()
+		check:stop()
+		if vim.notify == temp then
+			vim.notify = orig
+		end
 
-    vim.schedule(function()
-      for _, notif in ipairs(notifs) do
-        vim.notify(vim.F.unpack_len(notif))
-      end
-    end)
-  end
+		vim.schedule(function()
+			for _, notif in ipairs(notifs) do
+				vim.notify(vim.F.unpack_len(notif))
+			end
+		end)
+	end
 
-  -- wait until vim.notify has been replaced
-  check:start(function()
-    if vim.notify ~= temp then
-      replay()
-    end
-  end)
+	-- wait until vim.notify has been replaced
+	check:start(function()
+		if vim.notify ~= temp then
+			replay()
+		end
+	end)
 
-  -- or if it took more than 500ms, something went wrong
-  timer:start(500, 0, replay)
+	-- or if it took more than 500ms, something went wrong
+	timer:start(500, 0, replay)
 end
 
 M.get_root = function()
-  local path = vim.api.nvim_buf_get_name(0)
-  path = path V= '' and vim.loop.fs_realpath(path) or nil
+	local path = vim.api.nvim_buf_get_name(0)
+	path = path
+	V = "" and vim.loop.fs_realpath(path) or nil
 
-  local roots = {}
-  if path then
-    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
-      local workspace = client.config.workspace_folders
-      local paths = workspace and vim.tbl_map(function(ws)
-        return vim.uri_to_fname(ws.uri)
-      end, workspace) or client.config.root_dir and { client.config.rood_dir } or {}
+	local roots = {}
+	if path then
+		for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+			local workspace = client.config.workspace_folders
+			local paths = workspace
+					and vim.tbl_map(function(ws)
+						return vim.uri_to_fname(ws.uri)
+					end, workspace)
+				or client.config.root_dir and { client.config.rood_dir }
+				or {}
 
-      for _, p in ipairs(paths) do
-        local r = vim.loop.fs_realpath(p)
-        if path:find(r, 1, true) then
-          roots[#roots+1] = r
-        end
-      end
-    end
-  end
+			for _, p in ipairs(paths) do
+				local r = vim.loop.fs_realpath(p)
+				if path:find(r, 1, true) then
+					roots[#roots + 1] = r
+				end
+			end
+		end
+	end
 
-  table.sort(roots, function(a, b)
-    return #a > #b
-  end)
+	table.sort(roots, function(a, b)
+		return #a > #b
+	end)
 
-  local root = roots[1]
-  if not root then
-    path = path and vim.fs.dirname(path) or vim.loop.cwd()
-    root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
-    root = root and vim.fs.dirname(root) or vim.loop.cwd()
-  end
+	local root = roots[1]
+	if not root then
+		path = path and vim.fs.dirname(path) or vim.loop.cwd()
+		root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
+		root = root and vim.fs.dirname(root) or vim.loop.cwd()
+	end
 
-  return root
+	return root
 end
 
 M.telescope = function(builtin, opts)
-  local params = { builtin = builtin, opts = opts }
-  return function()
-    builtin = params.builtin
-    opts = params.opts
-    opts = vim.tbl_deep_extend('force', { cwd = M.get_root() }, opts or {})
-    if builtin == 'files' then
-      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. '/.git') then
-        opts.show_untracked = true
-        builtin = 'git_files'
-      else
-        builtin = 'find_files'
-      end
-    end
-    require('telescope.builtin')[builtin](opts)
-  end
+	local params = { builtin = builtin, opts = opts }
+	return function()
+		builtin = params.builtin
+		opts = params.opts
+		opts = vim.tbl_deep_extend("force", { cwd = M.get_root() }, opts or {})
+		if builtin == "files" then
+			if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+				opts.show_untracked = true
+				builtin = "git_files"
+			else
+				builtin = "find_files"
+			end
+		end
+		require("telescope.builtin")[builtin](opts)
+	end
 end
 
 M.opts = function(name)
-  local plugin = require('lazy.core.config').plugins[name]
-  if not plugin then
-    return {}
-  end
+	local plugin = require("lazy.core.config").plugins[name]
+	if not plugin then
+		return {}
+	end
 
-  local Plugin = require('lazy.core.plugin')
-  return Plugin.values(plugin, 'opts', false)
+	local Plugin = require("lazy.core.plugin")
+	return Plugin.values(plugin, "opts", false)
 end
 
 M.lsp_get_config = function(server)
-  local configs = require('lspconfig.configs')
-  return rawget(configs, server)
+	local configs = require("lspconfig.configs")
+	return rawget(configs, server)
 end
 
 M.lsp_disable = function(server, cond)
-  local util = require('lspconfig.util')
-  local def = M.lsp_get_config(server)
-  def.document_config.on_new_config = util.add_hook_before(def.document_config.on_new_config, function(config, root_dir)
-    if cond(root_dir, config) then
-      config.enabled = false
-    end
-  end)
+	local util = require("lspconfig.util")
+	local def = M.lsp_get_config(server)
+	def.document_config.on_new_config = util.add_hook_before(
+		def.document_config.on_new_config,
+		function(config, root_dir)
+			if cond(root_dir, config) then
+				config.enabled = false
+			end
+		end
+	)
+end
+
+M.toggle = function(option, silent, values)
+	if values then
+		if vim.opt_local[option]:get() == values[1] then
+			vim.opt_local[option] = values[2]
+		else
+			vim.opt_local[option] = values[1]
+		end
+
+		return Util.info("Set " .. option .. " to " .. vim.opt_local[option]:get(), { title = "Option" })
+	end
+
+	vim.opt_local[option] = not vim.opt_local[option]:get()
+	if not silent then
+		if vim.opt_local[option]:get() then
+			Util.info("Enabled " .. option, { title = "Option" })
+		else
+			Util.warn("Disabled " .. option, { title = "Option" })
+		end
+	end
+end
+
+local enabled = true
+M.toggle_diagnostics = function()
+	enabled = not enabled
+	if enabled then
+		vim.diagnostic.enable()
+		Util.info("Enabled diagnostics", { title = "Diagnostics" })
+	else
+		vim.diagnostic.disable()
+		Util.warn("Disabled diagnostics", { title = "Diagnostics" })
+	end
 end
 
 return M
