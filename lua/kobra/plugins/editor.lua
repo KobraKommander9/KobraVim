ef5df3af48d09c9caef613819b8024c49f376e64
Ed <edkilgore15@gmail.com>
2023-06-01 23:14:41 -0600

    Update telescope config

--- lua/kobra/plugins/editor.lua
+++ lua/kobra/plugins/editor.lua
@@ -163,20 +163,13 @@ M[#M + 1] = {
 		},
 	},
 	config = function(_, opts)
-		local e
-		if require("kobra.core").layouts.colemak then
-			e = "l"
-		else
-			e = "e"
-		end
-
 		local actions = require("telescope").extensions.file_browser.actions
 		local mappings = {}
 		if require("kobra.core").layouts.colemak then
 			mappings = {
 				i = {
 					["<c-a>"] = actions.create,
-					["<c-" .. e .. ">"] = actions.goto_home_dir,
+					["<c-l>"] = actions.goto_home_dir,
 					["<c-r>"] = actions.rename,
 					["<c-y>"] = actions.copy,
 					["<c-x>"] = actions.remove,
