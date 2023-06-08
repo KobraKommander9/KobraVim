62b176addd4a84bb56c124cf8abdf62c1d647d03
Ed <edkilgore15@gmail.com>
2023-06-07 21:02:12 -0600

    Update config for nvim-colorizer

--- lua/kobra/plugins/extras/editor/colors.lua
+++ lua/kobra/plugins/extras/editor/colors.lua
@@ -2,21 +2,14 @@ local M = {}
 
 M[#M + 1] = {
 	"norcalli/nvim-colorizer.lua",
+	event = "BufEnter",
 	keys = {
 		{ "<leader>bca", "<cmd>ColorizerAttachToBuffer<cr>", desc = "Colorize current buffer" },
 		{ "<leader>bcd", "<cmd>ColorizerDetachFromBuffer<cr>", desc = "Stop colorizing current buffer" },
 		{ "<leader>bcr", "<cmd>ColorizerReloadAllBuffers<cr>", desc = "Reload colorizer on all buffers" },
 		{ "<leader>bct", "<cmd>ColorizerToggle<cr>", desc = "Toggle colorizer" },
 	},
-	ft = { "css", "html", "javascript", "typescript", "lua", "vim" },
-	opts = {
-		"css",
-		"html",
-		"javascript",
-		"typescript",
-		"lua",
-		"vim",
-	},
+	config = true,
 }
 
 M[#M + 1] = {
