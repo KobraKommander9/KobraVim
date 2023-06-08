5cb7f525e74524a1aa074c3310e0f68ab592d12d
Ed <edkilgore15@gmail.com>
2023-06-07 20:57:28 -0600

    Add colorizer plugin

---
+++ lua/kobra/plugins/extras/editor/colors.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M[#M + 1] = {
+	"norcalli/nvim-colorizer.lua",
+	keys = {
+		{ "<leader>bca", "<cmd>ColorizerAttachToBuffer<cr>", desc = "Colorize current buffer" },
+		{ "<leader>bcd", "<cmd>ColorizerDetachFromBuffer<cr>", desc = "Stop colorizing current buffer" },
+		{ "<leader>bcr", "<cmd>ColorizerReloadAllBuffers<cr>", desc = "Reload colorizer on all buffers" },
+		{ "<leader>bct", "<cmd>ColorizerToggle<cr>", desc = "Toggle colorizer" },
+	},
+	ft = { "css", "html", "javascript", "typescript", "lua", "vim" },
+	opts = {
+		"css",
+		"html",
+		"javascript",
+		"typescript",
+		"lua",
+		"vim",
+	},
+}
+
+return M
