72d0b03a3a0ebd46c040da3473d0fb9415c3a5ca
Ed <ed.kilgore@tcnbroadcasting.com>
2023-06-22 09:33:00 -0600

    feat(marks): add telescope vim bookmarks

--- lua/kobra/plugins/extras/editor/marks.lua
+++ lua/kobra/plugins/extras/editor/marks.lua
@@ -6,4 +6,17 @@ M[#M + 1] = {
 	config = true,
 }
 
+M[#M + 1] = {
+	"tom-anders/telescope-vim-bookmarks.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+	},
+	keys = {
+		{ "<leader>fB", "<cmd>Telescope vim_bookmarks all", desc = "Find Bookmarks" },
+	},
+	config = function()
+		require("telescope").load_extension("vim_bookmarks")
+	end,
+}
+
 return M
