83ba51b695c5c56f051f38115ed46513c2ef9f70
Ed <ed.kilgore@tcnbroadcasting.com>
2023-11-20 10:28:40 -0700

    fix(plugins): update file to always open explorer in current buffer directory

--- lua/kobra/plugins/files.lua
+++ lua/kobra/plugins/files.lua
@@ -80,7 +80,7 @@ M[#M + 1] = {
 			"<leader>ff",
 			function()
 				if not MiniFiles.close() then
-					MiniFiles.open(vim.fn.expand("%:p:h"))
+					MiniFiles.open(vim.api.nvim_buf_get_name(0))
 				end
 			end,
 			desc = "View Files",
