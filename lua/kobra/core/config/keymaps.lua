5339b84ea2dbda3e88ec07287940ffdd99846311
Ed <ed.kilgore@tcnbroadcasting.com>
2024-07-29 14:06:14 -0600

    fix mappings

--- lua/kobra/core/config/keymaps.lua
+++ lua/kobra/core/config/keymaps.lua
@@ -15,12 +15,14 @@ Util.keymap("n", "<C-" .. Keys.k .. ">", "<C-w>k", { desc = "Go to upper window"
 Util.keymap("n", "<C-" .. Keys.l .. ">", "<C-w>l", { desc = "Go to right window" })
 
 -- move lines
+--[[
 Util.keymap("n", "<A-" .. Keys.j .. ">", "<cmd>m .+1<cr>==", { desc = "Move down" })
 Util.keymap("n", "<A-" .. Keys.k .. ">", "<cmd>m .-2<cr>==", { desc = "Move up" })
 Util.keymap("i", "<A-" .. Keys.j .. ">", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
 Util.keymap("i", "<A-" .. Keys.k .. ">", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 Util.keymap("v", "<A-" .. Keys.j .. ">", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 Util.keymap("v", "<A-" .. Keys.k .. ">", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+--]]
 
 -- clear search with <esc>
 Util.keymap({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
