1d0c4fa8faf5fb140e99d9825cbeeb40a1638480
Ed <ed.kilgore@tcnbroadcasting.com>
2024-07-29 22:24:50 -0600

    add comment plugins and notify

--- lua/kobra/plugins/ui.lua
+++ lua/kobra/plugins/ui.lua
@@ -4,11 +4,23 @@ M[#M + 1] = {
 	"xiyaowong/transparent.nvim",
 	lazy = false,
 	cond = require("kobra.core").ui.background == "transparent",
+}
+
+M[#M + 1] = {
+	"echasnovski/mini.notify",
 	opts = {
-		exclude_groups = {
-			"NotifyBackground",
+		lsp_progress = {
+			enable = false,
 		},
 	},
+	init = function()
+		-- when noice is not enabled, install notify on VeryLazy
+		if not KobraVim.has("noice.nvim") then
+			KobraVim.on_very_lazy(function()
+				vim.notify = require("mini.notify").make_notify()
+			end)
+		end
+	end,
 }
 
 return M
