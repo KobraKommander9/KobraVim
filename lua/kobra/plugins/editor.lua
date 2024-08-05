37b2cd6e38b4d83d33de93d62eaab477c1330d81
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-05 09:18:26 -0600

    add Hawtkeys

--- lua/kobra/plugins/editor.lua
+++ lua/kobra/plugins/editor.lua
@@ -2,91 +2,6 @@ local M = {}
 
 local Keys = require("kobra.core.keys")
 
--- next key clues
-M[#M + 1] = {
-	"echasnovski/mini.clue",
-	event = "VeryLazy",
-	opts = function(_, opts)
-		local miniclue = require("mini.clue")
-
-		local options = {
-			triggers = {
-				-- Leader triggers
-				{ mode = "n", keys = "<Leader>" },
-				{ mode = "x", keys = "<Leader>" },
-
-				-- Built-in completion
-				{ mode = "i", keys = "<C-x>" },
-
-				-- g key
-				{ mode = "n", keys = "g" },
-				{ mode = "x", keys = "g" },
-
-				-- marks
-				{ mode = "n", keys = "'" },
-				{ mode = "n", keys = "`" },
-				{ mode = "x", keys = "'" },
-				{ mode = "x", keys = "`" },
-
-				-- registers
-				{ mode = "n", keys = '"' },
-				{ mode = "x", keys = '"' },
-				{ mode = "i", keys = "<C-r>" },
-				{ mode = "c", keys = "<C-r>" },
-
-				-- window commands
-				{ mode = "n", keys = "<C-w>" },
-
-				-- `z` key
-				{ mode = "n", keys = "z" },
-				{ mode = "x", keys = "z" },
-
-				-- move
-				{ mode = "n", keys = "<leader>m" },
-				{ mode = "x", keys = "<leader>m" },
-
-				-- brackets
-				{ mode = "n", keys = "]" },
-				{ mode = "n", keys = "[" },
-			},
-			clues = {
-				miniclue.gen_clues.builtin_completion(),
-				miniclue.gen_clues.g(),
-				miniclue.gen_clues.marks(),
-				miniclue.gen_clues.registers(),
-				miniclue.gen_clues.windows({
-					submode_move = true,
-					submode_navigate = true,
-					submode_resize = true,
-				}),
-				miniclue.gen_clues.z(),
-
-				-- move
-				{ mode = "n", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
-				{ mode = "n", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
-				{ mode = "n", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
-				{ mode = "n", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
-				{ mode = "x", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
-				{ mode = "x", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
-				{ mode = "x", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
-				{ mode = "x", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
-
-				-- brackets
-				{ mode = "n", keys = "]b", postkeys = "]", desc = "next buffer" },
-				{ mode = "n", keys = "[b", postkeys = "[", desc = "previous buffer" },
-
-				-- clues
-				{ mode = "n", keys = "<leader>a", desc = "+Tabs" },
-				{ mode = "n", keys = "<leader>d", desc = "+Diagnostics" },
-				{ mode = "n", keys = "<leader>u", desc = "+UI" },
-				{ mode = "n", keys = "<leader>q", desc = "+Quit" },
-			},
-		}
-
-		return vim.tbl_extend("force", options, opts)
-	end,
-}
-
 -- move text
 M[#M + 1] = {
 	"echasnovski/mini.move",
@@ -117,42 +32,6 @@ M[#M + 1] = {
 	config = true,
 }
 
--- better escape
-M[#M + 1] = {
-	"max397574/better-escape.nvim",
-	event = "InsertEnter",
-	opts = {
-		default_mappings = true,
-		mappings = {
-			i = {
-				q = {
-					n = "<Esc>",
-				},
-			},
-			c = {
-				q = {
-					n = "<Esc>",
-				},
-			},
-			t = {
-				q = {
-					n = "<Esc>",
-				},
-			},
-			v = {
-				q = {
-					n = "<Esc>",
-				},
-			},
-			s = {
-				q = {
-					n = "<Esc>",
-				},
-			},
-		},
-	},
-}
-
 -- global search and replace
 M[#M + 1] = {
 	"nvim-pack/nvim-spectre",
@@ -167,38 +46,6 @@ M[#M + 1] = {
 		},
 	},
 	config = true,
-}
-
--- easily jump to any location and enhanced f/t motions for leap
-M[#M + 1] = {
-	"ggandor/flit.nvim",
-	keys = function()
-		local ret = {}
-		for _, key in ipairs({ "f", "F", "t", "T" }) do
-			ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
-		end
-		return ret
-	end,
-	opts = { labeled_modes = "nx" },
-}
-
-M[#M + 1] = {
-	"ggandor/leap.nvim",
-	keys = {
-		{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
-		{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-		{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-	},
-	config = function(_, opts)
-		local leap = require("leap")
-		for k, v in pairs(opts) do
-			leap.opts[k] = v
-		end
-
-		leap.add_default_mappings(true)
-		vim.keymap.del({ "x", "o" }, "x")
-		vim.keymap.del({ "x", "o" }, "X")
-	end,
 }
 
 -- file changes
---
+++ lua/kobra/plugins/keys.lua
@@ -0,0 +1,203 @@
+local M = {}
+
+local Core = require("kobra.core")
+local Keys = require("kobra.core.keys")
+
+-- better escape
+M[#M + 1] = {
+	"max397574/better-escape.nvim",
+	event = "InsertEnter",
+	opts = function(_, opts)
+		local options = {
+			default_mappings = true,
+		}
+
+		local colemak = {
+			mappings = {
+				i = {
+					q = {
+						n = "<Esc>",
+					},
+				},
+				c = {
+					q = {
+						n = "<Esc>",
+					},
+				},
+				t = {
+					q = {
+						n = "<Esc>",
+					},
+				},
+				v = {
+					q = {
+						n = "<Esc>",
+					},
+				},
+				s = {
+					q = {
+						n = "<Esc>",
+					},
+				},
+			},
+		}
+
+		if Core.layouts.colemak then
+			options = vim.tbl_deep_extend("force", options, colemak)
+		end
+
+		return vim.tbl_deep_extend("force", options, opts)
+	end,
+}
+
+-- next key clues
+M[#M + 1] = {
+	"echasnovski/mini.clue",
+	event = "VeryLazy",
+	opts = function(_, opts)
+		local miniclue = require("mini.clue")
+
+		local options = {
+			triggers = {
+				-- Leader triggers
+				{ mode = "n", keys = "<Leader>" },
+				{ mode = "x", keys = "<Leader>" },
+
+				-- Built-in completion
+				{ mode = "i", keys = "<C-x>" },
+
+				-- g key
+				{ mode = "n", keys = "g" },
+				{ mode = "x", keys = "g" },
+
+				-- marks
+				{ mode = "n", keys = "'" },
+				{ mode = "n", keys = "`" },
+				{ mode = "x", keys = "'" },
+				{ mode = "x", keys = "`" },
+
+				-- registers
+				{ mode = "n", keys = '"' },
+				{ mode = "x", keys = '"' },
+				{ mode = "i", keys = "<C-r>" },
+				{ mode = "c", keys = "<C-r>" },
+
+				-- window commands
+				{ mode = "n", keys = "<C-w>" },
+
+				-- `z` key
+				{ mode = "n", keys = "z" },
+				{ mode = "x", keys = "z" },
+
+				-- move
+				{ mode = "n", keys = "<leader>m" },
+				{ mode = "x", keys = "<leader>m" },
+
+				-- brackets
+				{ mode = "n", keys = "]" },
+				{ mode = "n", keys = "[" },
+			},
+			clues = {
+				miniclue.gen_clues.builtin_completion(),
+				miniclue.gen_clues.g(),
+				miniclue.gen_clues.marks(),
+				miniclue.gen_clues.registers(),
+				miniclue.gen_clues.windows({
+					submode_move = true,
+					submode_navigate = true,
+					submode_resize = true,
+				}),
+				miniclue.gen_clues.z(),
+
+				-- move
+				{ mode = "n", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
+				{ mode = "n", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
+				{ mode = "n", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
+				{ mode = "n", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
+				{ mode = "x", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
+				{ mode = "x", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
+				{ mode = "x", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
+				{ mode = "x", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
+
+				-- brackets
+				{ mode = "n", keys = "]b", postkeys = "]", desc = "next buffer" },
+				{ mode = "n", keys = "[b", postkeys = "[", desc = "previous buffer" },
+
+				-- clues
+				{ mode = "n", keys = "<leader>a", desc = "+Tabs" },
+				{ mode = "n", keys = "<leader>d", desc = "+Diagnostics" },
+				{ mode = "n", keys = "<leader>u", desc = "+UI" },
+				{ mode = "n", keys = "<leader>q", desc = "+Quit" },
+			},
+		}
+
+		return vim.tbl_deep_extend("force", options, opts)
+	end,
+}
+
+-- track key mappings
+M[#M + 1] = {
+	"tris203/hawtkeys.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	cmd = { "Hawtkeys", "HawtkeysAll", "HawtkeysDupes" },
+	keys = {
+		{ "<leader>k", desc = "Show key mappings" },
+		{ "<leader>K", desc = "Show all key mappings" },
+		{ "<leader>kd", desc = "Show duplicate key mappings" },
+	},
+	opts = function(_, opts)
+		local options = {
+			customMaps = {
+				["lazy"] = {
+					method = "lazy",
+				},
+			},
+		}
+
+		local colemak = {
+			keyboardLayout = "colemak",
+		}
+
+		if Core.layouts.colemak then
+			options = vim.tbl_deep_extend("force", options, colemak)
+		end
+
+		return vim.tbl_deep_extend("force", options, opts)
+	end,
+}
+
+-- easily jump to any location and enhanced f/t motions for leap
+M[#M + 1] = {
+	"ggandor/flit.nvim",
+	keys = function()
+		local ret = {}
+		for _, key in ipairs({ "f", "F", "t", "T" }) do
+			ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+		end
+		return ret
+	end,
+	opts = { labeled_modes = "nx" },
+}
+
+M[#M + 1] = {
+	"ggandor/leap.nvim",
+	keys = {
+		{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+		{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+		{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+	},
+	config = function(_, opts)
+		local leap = require("leap")
+		for k, v in pairs(opts) do
+			leap.opts[k] = v
+		end
+
+		leap.add_default_mappings(true)
+		vim.keymap.del({ "x", "o" }, "x")
+		vim.keymap.del({ "x", "o" }, "X")
+	end,
+}
+
+return M
