e18408a67494b74c25681e7a1b41591cb4e77c5d
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-16 14:32:06 -0600

    fix mappings

--- lua/kobra/core/keys/escape.lua
+++ lua/kobra/core/keys/escape.lua
@@ -173,8 +173,7 @@ local function check_key(key)
 		end
 	end
 
-	vim.notify("mode: " .. mode)
-	for first_key in pairs(settings.mappings[mode]) do
+	for first_key in pairs(settings.mappings[mode] or {}) do
 		if key == first_key then
 			record_key(key)
 			return
