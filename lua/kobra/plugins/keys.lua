40a2aa83ef2060512e49ea172891d760f37c95f4
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-16 18:28:20 -0600

    update clue triggers

--- lua/kobra/plugins/keys.lua
+++ lua/kobra/plugins/keys.lua
@@ -66,22 +66,22 @@ M[#M + 1] = {
 				miniclue.gen_clues.z(),
 
 				-- move
-				{ mode = "n", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
-				{ mode = "n", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
-				{ mode = "n", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
-				{ mode = "n", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
-				{ mode = "x", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
-				{ mode = "x", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
-				{ mode = "x", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
-				{ mode = "x", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
+				{ mode = "n", keys = "<leader>mh", postkeys = "<leader>m" },
+				{ mode = "n", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m" },
+				{ mode = "n", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m" },
+				{ mode = "n", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m" },
+				{ mode = "x", keys = "<leader>mh", postkeys = "<leader>m" },
+				{ mode = "x", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m" },
+				{ mode = "x", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m" },
+				{ mode = "x", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m" },
 
 				-- tabs
-				{ mode = "n", keys = "<leader>am" .. Keys.j, postkeys = "<leader>am", desc = "Move tab right" },
-				{ mode = "n", keys = "<leader>am" .. Keys.k, postkeys = "<leader>am", desc = "Move tab left" },
+				{ mode = "n", keys = "<leader>am" .. Keys.j, postkeys = "<leader>am" },
+				{ mode = "n", keys = "<leader>am" .. Keys.k, postkeys = "<leader>am" },
 
 				-- brackets
-				{ mode = "n", keys = "]b", postkeys = "]", desc = "next buffer" },
-				{ mode = "n", keys = "[b", postkeys = "[", desc = "previous buffer" },
+				{ mode = "n", keys = "]b", postkeys = "]" },
+				{ mode = "n", keys = "[b", postkeys = "[" },
 
 				-- clues
 				{ mode = "n", keys = "<leader>a", desc = "+Tabs" },
