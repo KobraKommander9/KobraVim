34900183cd04440b24d9b1d805e3790b22881f04
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-16 20:26:41 -0600

    update configs

--- lua/kobra/plugins/editor.lua
+++ lua/kobra/plugins/editor.lua
@@ -11,24 +11,20 @@ M[#M + 1] = {
 		{
 			"echasnovski/mini.clue",
 			opts = function(_, opts)
-				local clues = {
-					{ mode = "n", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
-					{ mode = "n", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
-					{ mode = "n", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
-					{ mode = "n", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
-					{ mode = "x", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
-					{ mode = "x", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
-					{ mode = "x", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
-					{ mode = "x", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
+				local options = {
+					clues = {
+						{ mode = "n", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
+						{ mode = "n", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
+						{ mode = "n", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
+						{ mode = "n", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
+						{ mode = "x", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
+						{ mode = "x", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
+						{ mode = "x", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
+						{ mode = "x", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
+					},
 				}
 
-				if opts.clues == nil then
-					opts.clues = {}
-				end
-
-				for _, clue in ipairs(clues) do
-					table.insert(opts.clues, clue)
-				end
+				return vim.tbl_deep_extend("force", options, opts)
 			end,
 		},
 	},
