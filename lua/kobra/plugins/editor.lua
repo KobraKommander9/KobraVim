b70b1acc272186c27595842db9d42a2b7d4985e9
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-18 22:19:19 -0600

    Update move config

--- lua/kobra/plugins/editor.lua
+++ lua/kobra/plugins/editor.lua
@@ -77,22 +77,6 @@ M[#M + 1] = {
 		}
 		return vim.tbl_deep_extend("force", options, opts)
 	end,
-	-- config = function(_, opts)
-	-- 	require("mini.move").setup(opts)
-	-- 	-- KobraVim.clue({
-	-- 	-- 	clues = {
-	-- 	-- 		{ mode = "n", keys = "<leader>m", desc = "+Move" },
-	-- 	-- 		{ mode = "n", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
-	-- 	-- 		{ mode = "n", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
-	-- 	-- 		{ mode = "n", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
-	-- 	-- 		{ mode = "n", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
-	-- 	-- 		{ mode = "x", keys = "<leader>mh", postkeys = "<leader>m", desc = "Move left" },
-	-- 	-- 		{ mode = "x", keys = "<leader>m" .. Keys.l, postkeys = "<leader>m", desc = "Move right" },
-	-- 	-- 		{ mode = "x", keys = "<leader>m" .. Keys.j, postkeys = "<leader>m", desc = "Move down" },
-	-- 	-- 		{ mode = "x", keys = "<leader>m" .. Keys.k, postkeys = "<leader>m", desc = "Move up" },
-	-- 	-- 	},
-	-- 	-- })
-	-- end,
 }
 
 -- better bracket jumps
