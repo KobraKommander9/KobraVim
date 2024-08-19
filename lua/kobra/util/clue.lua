aac6e4de0e4e4afa26e3298c0c8bf68c2f4a9677
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-18 22:37:01 -0600

    update clues

--- lua/kobra/util/clue.lua
+++ lua/kobra/util/clue.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.options(clues, triggers)
 	return function(_, opts)
 		opts = opts or {}
-		opts.clues = vim.tbl_deep_extend("force", clues or {}, opts.clues or {})
-		opts.triggers = vim.tbl_deep_extend("force", triggers or {}, opts.triggers or {})
+		opts.clues = vim.tbl_deep_extend("force", opts.clues or {}, clues or {})
+		opts.triggers = vim.tbl_deep_extend("force", opts.triggers or {}, triggers or {})
 	end
 end
 
