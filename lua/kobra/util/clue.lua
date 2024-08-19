cf7f243ed89df942fa9d3ea7210acd51a8b674dd
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-18 21:37:05 -0600

    add clue configs

--- lua/kobra/util/clue.lua
+++ lua/kobra/util/clue.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup(opts)
 	local MiniClue = require("mini.clue")
 	local config = vim.tbl_deep_extend("force", MiniClue.config or {}, opts)
-	MiniClue.apply_config(config)
+	MiniClue.setup(config)
 end
 
 setmetatable(M, {
