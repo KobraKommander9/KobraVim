85b4ebc7a9e0e6359497c801d325e15277965324
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-19 20:42:23 -0600

    update ui config

--- lua/kobra/util/ui.lua
+++ lua/kobra/util/ui.lua
@@ -57,4 +57,25 @@ function M.foldexpr()
 	return "0"
 end
 
+function M.fg(name)
+	local color = M.color(name)
+	return color and { fg = color } or nil
+end
+
+function M.color(name, bg)
+	local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name, link = false })
+		or vim.api.nvim_get_hl_by_name(name, true)
+
+	local color = nil
+	if hl then
+		if bg then
+			color = hl.bg or hl.background
+		else
+			color = hl.fg or hl.foreground
+		end
+	end
+
+	return color and string.format("#%06x", color) or nil
+end
+
 return M
