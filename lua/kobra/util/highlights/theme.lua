e0e3fff32d6c8cfa1e0aa25a91cc27ab9036d8d9
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-22 16:51:15 -0600

    update colors

--- lua/kobra/util/highlights/theme.lua
+++ lua/kobra/util/highlights/theme.lua
@@ -94,11 +94,12 @@ end
 -- get colors to generate theme
 local function get_colors()
 	local colors = {
-		normal = KobraColors.utils.extract_color_from_hllist(
-			"bg",
-			{ "PmenuSel", "PmenuThumb", "TabLineSel" },
-			"#000000"
-		),
+		-- normal = KobraColors.utils.extract_color_from_hllist(
+		-- 	"bg",
+		-- 	{ "PmenuSel", "PmenuThumb", "TabLineSel" },
+		-- 	"#000000"
+		-- ),
+    normal = KobraColors.utils.extract_color_from_hlliT("fg", { "Constant" }, "#000000"),
 		insert = KobraColors.utils.extract_color_from_hllist("fg", { "String", "MoreMsg" }, "#000000"),
 		replace = KobraColors.utils.extract_color_from_hllist("fg", { "Number", "Type" }, "#000000"),
 		visual = KobraColors.utils.extract_color_from_hllist("fg", { "Special", "Boolean", "Constant" }, "#000000"),
