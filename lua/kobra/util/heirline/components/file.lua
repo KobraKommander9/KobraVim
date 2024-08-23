b20f4ed14527b86e2023e00388c58646a813e169
Ed <ed.kilgore@tcnbroadcasting.com>
2024-08-23 08:42:44 -0600

    update file component

--- lua/kobra/util/heirline/components/file.lua
+++ lua/kobra/util/heirline/components/file.lua
@@ -3,91 +3,93 @@ local M = {}
 local conditions = require("heirline.conditions")
 
 function M.work_dir()
-  return {
-    provider = function()
-      local icon = (vim.fn.haslocaldir(0) == 1 and "l" or "g") .. " " .. Kobra.config.icons.kinds.Folder
-      local cwd = vim.fn.getcwd(0)
-      cwd = vim.fn.fnamemodify(cwd, ":~")
-      
-      if not conditions.width_percent_below(#cwd, 0.25) then
-        cwd = vim.fn.pathshorten(cwd)
-      end
-      
-      local trail = cwd:sub(-1) == "/" and " " or "/ "
-      return icon .. cwd .. trail
-    end,
-    hl = "kobra_term_gray",
-  }
+	return {
+		provider = function()
+			local icon = (vim.fn.haslocaldir(0) == 1 and "l" or "g") .. " " .. Kobra.config.icons.kinds.Folder
+			local cwd = vim.fn.getcwd(0)
+			cwd = vim.fn.fnamemodify(cwd, ":~")
+
+			if not conditions.width_percent_below(#cwd, 0.25) then
+				cwd = vim.fn.pathshorten(cwd)
+			end
+
+			local trail = cwd:sub(-1) == "/" and " " or "/ "
+			return icon .. cwd .. trail
+		end,
+		hl = "kobra_term_gray",
+	}
 end
 
 function M.file_icon()
-  return {
-    init = function(self)
-      if not self.filename then
-        self.filename = vim.api.nvim_buf_get_name(0)
-      end
-      
-      local filename = self.filename
-      local extension = vim.fn.fnamemodify(filename, ":e")
-      self.icon_data = require("mini.icons").get("file", filename .. extension)
-    end,
-    provider = function(self)
-      return " " .. (self.icon_data.icon and (self.icon_data.icon .. " ") or Kobra.config.icons.kinds.File .. " ")
-    end,
-    hl = "kobra_term_gray",
-  }
+	return {
+		init = function(self)
+			if not self.filename then
+				self.filename = vim.api.nvim_buf_get_name(0)
+			end
+
+			local filename = self.filename
+			local extension = vim.fn.fnamemodify(filename, ":e")
+			self.icon_data = require("mini.icons").get("file", filename .. extension)
+		end,
+		provider = function(self)
+			return " " .. (self.icon_data.icon and (self.icon_data.icon .. " ") or Kobra.config.icons.kinds.File .. " ")
+		end,
+		hl = "kobra_term_gray",
+	}
 end
 
 function M.file_name()
-  return {
-    provider = function(self)
-      local filename = vim.fn.fnamemodify(self.filename, ":.")
-      if filename == "" then
-        return "[No Name]"
-      end
-      
-      if not conditions.width_percent_below(#filename, 0.25) then
-        filename = vim.fn.pathshorten(filename)
-      end
-      
-      return filename
-    end,
-    hl = "kobra_term_gray",
-  }
+	return {
+		provider = function(self)
+			local filename = vim.fn.fnamemodify(self.filename, ":.")
+			if filename == "" then
+				return "[No Name]"
+			end
+
+			if not conditions.width_percent_below(#filename, 0.25) then
+				filename = vim.fn.pathshorten(filename)
+			end
+
+			return filename
+		end,
+		hl = "kobra_term_gray",
+	}
 end
 
 function M.file_flags()
-  return {
-    {
-      condition = function()
-        return vim.bo.modified
-      end,
-      provider = "[+]",
-      hl = "kobra_term_green",
-    },
-    {
-      condition = function()
-        return vim.bo.readonly
-      end,
-      provider = "[RO]",
-      hl = "kobra_term_red",
-    },
-  }
+	return {
+		{
+			condition = function()
+				return vim.bo.modified
+			end,
+			provider = "[+]",
+			hl = "kobra_term_green",
+		},
+		{
+			condition = function()
+				return vim.bo.readonly
+			end,
+			provider = "[RO]",
+			hl = "kobra_term_red",
+		},
+	}
 end
 
 function M.component()
-  return {
-    {
-      init = function(self)
-        self.filename = vim.api.nvim_buf_get_name(0)
-      end,
-    },
-    M.work_dir(),
-    M.file_icon(),
-    M.file_name(),
-    M.file_flags(),
-    { provider = "%<" },
-  }
+	return {
+		{
+			init = function(self)
+				self.filename = vim.api.nvim_buf_get_name(0)
+			end,
+		},
+		{ provider = "" },
+		M.work_dir(),
+		M.file_icon(),
+		M.file_name(),
+		M.file_flags(),
+		{ provider = "" },
+		{ provider = "%<" },
+	}
 end
 
 return M
