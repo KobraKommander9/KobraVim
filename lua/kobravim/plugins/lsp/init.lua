4ba5eb700dcad32e695170cefedce4906ba4eb62
Ed <ed.kilgore@tcnbroadcasting.com>
2025-04-21 09:27:47 -0600

    fix lsp opts

--- lua/kobravim/plugins/lsp/init.lua
+++ lua/kobravim/plugins/lsp/init.lua
@@ -72,6 +72,15 @@ M[#M + 1] = {
 					},
 				},
 			},
+			setup = {
+				-- example to setup with typescript.nvim
+				-- tsserver = function(_, opts)
+				--   require("typescript").setup({ server = opts })
+				--   return true
+				-- end,
+				-- Specify * to use this function as a fallback for any server
+				-- ["*"] = function(server, opts) end,
+			},
 		}
 	end,
 	config = function(_, opts)
