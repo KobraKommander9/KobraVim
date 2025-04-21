2ce0953324fabcc4e257c0bc8aa2ff8aa12f9485
Ed <ed.kilgore@tcnbroadcasting.com>
2025-04-21 10:14:49 -0600

    fix pick

--- lua/kobravim/plugins/pick.lua
+++ lua/kobravim/plugins/pick.lua
@@ -27,13 +27,13 @@ M[#M + 1] = {
 		},
 
 		-- Files
-		{ "<leader>fs", Kobra.pick.builtin("files"), desc = "Search files" },
+		{ "<leader>fs", KobraVim.pick.builtin("files"), desc = "Search files" },
 
 		-- Search
-		{ "<leader>sg", Kobra.pick.builtin("grep_live"), desc = "Search (grep live)" },
-		{ "<leader>sG", Kobra.pick.builtin("grep"), desc = "Search (grep)" },
-		{ "<leader>sH", Kobra.pick.builtin("help"), desc = "Search help tags" },
-		{ "<leader>sr", Kobra.pick.builtin("resume"), desc = "Resume search" },
+		{ "<leader>sg", KobraVim.pick.builtin("grep_live"), desc = "Search (grep live)" },
+		{ "<leader>sG", KobraVim.pick.builtin("grep"), desc = "Search (grep)" },
+		{ "<leader>sH", KobraVim.pick.builtin("help"), desc = "Search help tags" },
+		{ "<leader>sr", KobraVim.pick.builtin("resume"), desc = "Resume search" },
 	},
 	opts = function(_, opts)
 		opts = opts or {}
@@ -59,44 +59,48 @@ M[#M + 1] = {
 	},
 	cmd = { "Pick" },
 	keys = {
-		{ "<leader>sc", Kobra.pick.extra("history", "{ scope = '/' }"), desc = "Search history" },
-		{ "<leader>sC", Kobra.pick.extra("history", "{ scope = ':' }"), desc = "Search command history" },
-		{ "<leader>sd", Kobra.pick.extra("diagnostic", "{ scope = 'current' }"), desc = "Search diagnostics" },
-		{ "<leader>sD", Kobra.pick.extra("diagnostic"), desc = "Search diagnostics (all)" },
-		{ "<leader>sm", Kobra.pick.extra("list", "{ scope = 'change' }"), desc = "Search changelist" },
-		{ "<leader>sM", Kobra.pick.extra("marks"), desc = "Search marks" },
-		{ "<leader>so", Kobra.pick.extra("options"), desc = "Search options" },
-		{ "<leader>sq", Kobra.pick.extra("list", "{ scope = 'quickfix' }"), desc = "Search quickfix" },
-		{ "<leader>sT", Kobra.pick.extra("treesitter"), desc = "Search treesitter" },
-		{ "<leader>sz", Kobra.pick.extra("spellsuggest"), desc = "Search spelling suggestions" },
+		{ "<leader>sc", KobraVim.pick.extra("history", "{ scope = '/' }"), desc = "Search history" },
+		{ "<leader>sC", KobraVim.pick.extra("history", "{ scope = ':' }"), desc = "Search command history" },
+		{ "<leader>sd", KobraVim.pick.extra("diagnostic", "{ scope = 'current' }"), desc = "Search diagnostics" },
+		{ "<leader>sD", KobraVim.pick.extra("diagnostic"), desc = "Search diagnostics (all)" },
+		{ "<leader>sm", KobraVim.pick.extra("list", "{ scope = 'change' }"), desc = "Search changelist" },
+		{ "<leader>sM", KobraVim.pick.extra("marks"), desc = "Search marks" },
+		{ "<leader>so", KobraVim.pick.extra("options"), desc = "Search options" },
+		{ "<leader>sq", KobraVim.pick.extra("list", "{ scope = 'quickfix' }"), desc = "Search quickfix" },
+		{ "<leader>sT", KobraVim.pick.extra("treesitter"), desc = "Search treesitter" },
+		{ "<leader>sz", KobraVim.pick.extra("spellsuggest"), desc = "Search spelling suggestions" },
 
 		-- Buffers
-		{ "<leader>bl", Kobra.pick.extra("buf_lines"), desc = "Search buffer lines" },
+		{ "<leader>bl", KobraVim.pick.extra("buf_lines"), desc = "Search buffer lines" },
 
 		-- Files
 		{
 			"<leader>fS",
-			Kobra.pick.extra("explorer", "{ cwd = vim.fn.expand('%:p:h') }"),
+			KobraVim.pick.extra("explorer", "{ cwd = vim.fn.expand('%:p:h') }"),
 			desc = "Search files (explorer)",
 		},
-		{ "<leader>fo", Kobra.pick.extra("oldfiles"), desc = "Search old files" },
+		{ "<leader>fo", KobraVim.pick.extra("oldfiles"), desc = "Search old files" },
 
 		-- Git
-		{ "<leader>gb", Kobra.pick.extra("git_branches"), desc = "Search git branches" },
-		{ "<leader>gc", Kobra.pick.extra("git_commits"), desc = "Search git commits" },
-		{ "<leader>gf", Kobra.pick.extra("git_files"), desc = "Search git files" },
-		{ "<leader>gh", Kobra.pick.extra("git_hunks"), desc = "Search git hunks" },
+		{ "<leader>gb", KobraVim.pick.extra("git_branches"), desc = "Search git branches" },
+		{ "<leader>gc", KobraVim.pick.extra("git_commits"), desc = "Search git commits" },
+		{ "<leader>gf", KobraVim.pick.extra("git_files"), desc = "Search git files" },
+		{ "<leader>gh", KobraVim.pick.extra("git_hunks"), desc = "Search git hunks" },
 
 		-- LSP
-		{ "<leader>ld", Kobra.pick.extra("lsp", "{ scope = 'declaration' }"), desc = "Search LSP declarations" },
-		{ "<leader>lD", Kobra.pick.extra("lsp", "{ scope = 'definition' }"), desc = "Search LSP definitions" },
-		{ "<leader>li", Kobra.pick.extra("lsp", "{ scope = 'implementation' }"), desc = "Search LSP implementations" },
-		{ "<leader>lr", Kobra.pick.extra("lsp", "{ scope = 'references' }"), desc = "Search LSP references" },
-		{ "<leader>ls", Kobra.pick.extra("lsp", "{ scope = 'document_symbol' }"), desc = "Search LSP symbols" },
-		{ "<leader>lt", Kobra.pick.extra("lsp", "{ scope = 'type_definition' }"), desc = "Search LSP types" },
+		{ "<leader>ld", KobraVim.pick.extra("lsp", "{ scope = 'declaration' }"), desc = "Search LSP declarations" },
+		{ "<leader>lD", KobraVim.pick.extra("lsp", "{ scope = 'definition' }"), desc = "Search LSP definitions" },
+		{
+			"<leader>li",
+			KobraVim.pick.extra("lsp", "{ scope = 'implementation' }"),
+			desc = "Search LSP implementations",
+		},
+		{ "<leader>lr", KobraVim.pick.extra("lsp", "{ scope = 'references' }"), desc = "Search LSP references" },
+		{ "<leader>ls", KobraVim.pick.extra("lsp", "{ scope = 'document_symbol' }"), desc = "Search LSP symbols" },
+		{ "<leader>lt", KobraVim.pick.extra("lsp", "{ scope = 'type_definition' }"), desc = "Search LSP types" },
 		{
 			"<leader>lw",
-			Kobra.pick.extra("lsp", "{ scope = 'workspace_symbol' }"),
+			KobraVim.pick.extra("lsp", "{ scope = 'workspace_symbol' }"),
 			desc = "Search LSP workspace symbols",
 		},
 	},
