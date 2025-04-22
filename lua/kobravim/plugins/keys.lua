c376e2288443fc6d34920ed301b46212bd37e82e
Ed <ed.kilgore@tcnbroadcasting.com>
2025-04-22 09:22:01 -0600

    Update stuff

--- lua/kobravim/plugins/keys.lua
+++ lua/kobravim/plugins/keys.lua
@@ -17,10 +17,10 @@ M[#M + 1] = {
 -- next key clues
 M[#M + 1] = {
 	"echasnovski/mini.clue",
-	lazy = false,
+	event = "VeryLazy",
 	opts = KobraVim.mini.clue_options(function()
 		local miniclue = require("mini.clue")
-		local keys = KobraVim.keys
+		-- local keys = KobraVim.keys
 
 		return {
 			miniclue.gen_clues.builtin_completion(),
@@ -35,8 +35,8 @@ M[#M + 1] = {
 			miniclue.gen_clues.z(),
 
 			-- tabs
-			{ mode = "n", keys = "<leader>am" .. keys.j, postkeys = "<leader>am", desc = "Move tab down" },
-			{ mode = "n", keys = "<leader>am" .. keys.k, postkeys = "<leader>am", desc = "Move tab up" },
+			-- { mode = "n", keys = "<leader>am" .. keys.j, postkeys = "<leader>am", desc = "Move tab down" },
+			-- { mode = "n", keys = "<leader>am" .. keys.k, postkeys = "<leader>am", desc = "Move tab up" },
 
 			-- clues
 			{ mode = "n", keys = "<leader>a", desc = "+Tabs" },
