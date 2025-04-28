6eabe99266d3af56bac6d8eff53e27d41f5b3960
Ed <ed.kilgore@tcnbroadcasting.com>
2025-04-28 09:15:19 -0600

    update flash

--- lua/kobravim/plugins/keys.lua
+++ lua/kobravim/plugins/keys.lua
@@ -88,31 +88,13 @@ M[#M + 1] = {
 -- easily jump to any location
 M[#M + 1] = {
 	"folke/flash.nvim",
+  -- stylua: ignore
 	keys = {
-		{
-			"<cr>",
-			mode = { "n", "x", "o" },
-			function()
-				require("flash").jump()
-			end,
-			desc = "Flash",
-		},
-		{
-			"r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-		{
-			"<c-c>",
-			mode = { "c" },
-			function()
-				require("flash").toggle()
-			end,
-			desc = "Toggle Flash Search",
-		},
+		{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+		{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+		{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
 	},
 }
 
