b85c3d978454f26fc4bbbbbbc7ed065ae5e44ae8
Ed <ed.kilgore@tcnbroadcasting.com>
2026-01-13 12:09:51 -0500

    feat(plugins): remove mini.git

--- lua/kobravim/plugins/coding.lua
+++ lua/kobravim/plugins/coding.lua
@@ -1,23 +1,5 @@
 local M = {}
 
-M[#M + 1] = {
-	"echasnovski/mini-git",
-	event = "VeryLazy",
-	config = function(_, opts)
-		require("mini.git").setup(opts)
-
-		local format_summary = function(data)
-			local summary = vim.b[data.buf].minigit_summary
-			vim.b[data.buf].minigit_summary_string = summary.head_name or ""
-		end
-
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "MiniGitUpdated",
-			callback = format_summary,
-		})
-	end,
-}
-
 -- better a/i textobjects
 M[#M + 1] = {
 	"echasnovski/mini.ai",
