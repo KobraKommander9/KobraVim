fe1a986339107bcc3c29a37938dcc26ec606d64b
Ed <ed.kilgore@tcnbroadcasting.com>
2026-01-25 14:49:09 -0500

    update statusline

--- lua/kobravim/util/bars/palette.lua
+++ lua/kobravim/util/bars/palette.lua
@@ -1,35 +1,39 @@
 local M = {}
 
 local function get_hl(name, attr)
-	local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
-	local color = hl[attr or "fg"]
-	return color and string.format("#%06x", color) or "NONE"
+	local ok, h = pcall(vim.api.nvim_get_hl, 0, { name = name, link = false })
+	if ok then
+		return string.format("#%06x", h[attr or "fg"])
+	end
 end
 
 local colors = {
-	blue = get_hl("Function"),
-	bg = get_hl("StatusLine", "bg"),
-	bright_bg = get_hl("Folded", "bg"),
-	green = get_hl("DiagnosticOk"),
-	magenta = get_hl("Statement"),
-	orange = get_hl("DiagnosticWarn"),
-	red = get_hl("DiagnosticError"),
+	bg = get_hl("StatusLine", "bg") or "NONE",
+	bright_bg = get_hl("Folded", "bg") or "NONE",
+
+	active = get_hl("Function") or get_hl("Identifier") or "NONE",
+	danger = get_hl("Error") or get_hl("DiagnosticError") or "NONE",
+	directive = get_hl("Keyword") or get_hl("Statement") or "NONE",
+	literal = get_hl("String") or get_hl("Constant") or "NONE",
+	number = get_hl("Number") or "NONE",
+	special = get_hl("Special") or "NONE",
+	type = get_hl("Type") or get_hl("Keyword") or "NONE",
 }
 
 local mode_map = {
-	n = colors.blue,
-	i = colors.green,
-	v = colors.magenta,
-	V = colors.magenta,
-	["\22"] = colors.magenta,
-	c = colors.orange,
-	s = colors.orange,
-	S = colors.orange,
-	["\19"] = colors.orange,
-	R = colors.red,
-	r = colors.red,
-	["!"] = colors.red,
-	t = colors.green,
+	n = colors.active,
+	i = colors.literal,
+	v = colors.type,
+	V = colors.type,
+	["\22"] = colors.type,
+	c = colors.directive,
+	s = colors.special,
+	S = colors.special,
+	["\19"] = colors.special,
+	R = colors.danger,
+	r = colors.danger,
+	["!"] = colors.danger,
+	t = colors.number,
 }
 
 for name, hex in pairs(colors) do
