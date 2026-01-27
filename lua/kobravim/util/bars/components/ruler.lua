9208e6a3fca7684b1f25a609e1e2dd88c6aad119
Ed <ed.kilgore@tcnbroadcasting.com>
2026-01-27 15:59:00 -0500

    update statusline ruler

--- lua/kobravim/util/bars/components/ruler.lua
+++ lua/kobravim/util/bars/components/ruler.lua
@@ -2,6 +2,5 @@ return {
 	-- %l = current line number
 	-- %L = number of lines in the buffer
 	-- %c = current column
-	-- %P = percentage through the file
-	provider = "(%l/%L):%-3c %P",
+	provider = "(%l/%L):%-3c",
 }
