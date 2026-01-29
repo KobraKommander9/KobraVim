6d6f5353536a59ae7f6577c3076eb26f29d7411c
Ed <ed.kilgore@tcnbroadcasting.com>
2026-01-29 11:11:31 -0500

    setup notebook management

--- lua/kobravim/plugins/extras/lang/markdown.lua
+++ lua/kobravim/plugins/extras/lang/markdown.lua
@@ -26,9 +26,30 @@ local function delete_notebook()
 	end)
 end
 
+local function register_notebook()
+	vim.ui.input({
+		prompt = "Enter Notebook Path: ",
+		default = vim.fn.getcwd(),
+	}, function(path)
+		if not path then
+			return
+		end
+
+		vim.ui.input({
+			prompt = "Enter Notebook Title: ",
+			default = vim.fn.fnamemodify(path, ":t"),
+		}, function(title)
+			if title then
+				require("bookwyrm").api.register_notebook({ path = path, title = title })
+			end
+		end)
+	end)
+end
+
 local function search_notebooks()
 	notebook_picker(function(item)
 		require("bookwyrm").api.select_notebook(item.nb_id)
+		vim.notify("Active: " .. item.title)
 	end)
 end
 
@@ -41,14 +62,18 @@ M[#M + 1] = {
 			"nvim-mini/mini.clue",
 			opts = KobraVim.mini.clue_options({
 				{ mode = "n", keys = "<leader>j", desc = "+Journals" },
+				{ mode = "n", keys = "<leader>jr", desc = "+Registry" },
 			}),
 		},
 	},
-	cmd = { "BookwyrmRegister" },
+	cmd = { "BookwyrmNotebookRegister", "BookwyrmNotebookRename", "BookwyrmNotebookSetDefault" },
 	keys = {
-		{ "<leader>jD", delete_notebook, desc = "Delete notebooks" },
-		{ "<leader>jR", "<cmd>BookwyrmRegister<cr>", desc = "Register current dir" },
-		{ "<leader>jS", search_notebooks, desc = "Search notebooks" },
+		{ "<leader>jrd", "<cmd>BookwyrmNotebookSetDefault<cr>", desc = "Set active default" },
+		{ "<leader>jrD", delete_notebook, desc = "Delete notebooks" },
+		{ "<leader>jrN", register_notebook, desc = "Register notebook" },
+		{ "<leader>jrn", "<cmd>BookwyrmNotebookRegister<cr>", desc = "Register current dir" },
+		{ "<leader>jrr", "<cmd>BookwyrmNotebookRename<cr>", desc = "Rename active notebook" },
+		{ "<leader>jrs", search_notebooks, desc = "Search notebooks" },
 	},
 	config = true,
 }
