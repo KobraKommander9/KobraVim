2c02c5debd85d07d91bbe7fc88bd557dd0e4b1d9
drakonix <drakonix.dev@gmail.com>
2026-02-19 22:19:09 -0500

    update config opts

--- lua/kobravim/config/options.lua
+++ lua/kobravim/config/options.lua
@@ -47,7 +47,7 @@ opt.showmode = false -- Dont show mode since we have a statusline
 opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
+opt.smartindent = false
 opt.spelllang = { "en" }
 opt.spelloptions:append("noplainbuffer")
 opt.splitbelow = true -- Put new windows below current
